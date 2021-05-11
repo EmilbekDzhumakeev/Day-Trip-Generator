bc33b36237fcd12a0302e4c9825649e3655f8c79
EmilbekDzhumakeev <emilbek.dzhumakeev17@gmail.com>
2021-05-11 12:33:32 -0400

    Created arrays for my day trip and random fuction, first test sucsessful

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-"use strict"; 
+"use strict";  
+  
+
+    let destinations = ["Tampa", "Orlando", "Brandon", "St.Petersburg", "Miami"];
+    let restaurant = ["IHOP", "Chili's", "Applebee's", "Cheesecake Factory", "Outback"];
+    let transport = ["Ferry", "Car", "Train", "Bus","Horse"]; 
+    let entertainment = ["Concert", "Movies", "Stand up comedy", "Theater", "Museum"];
+ 
+    function random(item){
+      let i = Math.floor(Math.random() * 6); 
+      return item[i];
+    } 
+
+   console.log("Your trip: "+random(destinations)+" "+random(restaurant)+" "+random(transport) +" "+random(entertainment));
